1a22ceb438ff7953f527345a5a513a2f02cfbc01
Krzysztof <krzysztof.wysocki@gmail.com>
2023-03-15 21:24:20 +0100

    Initial Commit

---
+++ GPTalkie/GPTalkieApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPTalkieApp.swift
+//  GPTalkie
+//
+//  Created by kwysocki on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GPTalkieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
